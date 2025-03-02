fbaf24929eab3f76902e2d38bc1e7fad39305c97
Jônatas nascimento dos santos <jonatasnsantos@hotmail.com>
2025-03-02 20:02:51 -0300

    Update registrarAtividadeControler.php

--- agendaTINT/app/Http/Controllers/registrarAtividadeControler.php
+++ agendaTINT/app/Http/Controllers/registrarAtividadeControler.php
@@ -1,25 +1,27 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers; // nome
 
-use Illuminate\Http\Request;
-use App\Models\modelAgenda;
+use Illuminate\Http\Request; // puxa o request com form
+use App\Models\modelAgenda; // conecta com a model
 
 class registrarAtividadeControler extends Controller
 {
     public function index(){
         $dados = modelAgenda::all(); //tras todos os dados da tabela
-        return view('paginas.cadastrar')->With('dados',$dados);
+        return view('paginas.cadastrar')->With('dados',$dados); // retorna a visualizacao com dados
     }//fim do método -retornar dados
-    public function store(request $request){
-        $data = $request->input('dataEvento');
+    public function store(request $request){        
+        $data = $request->input('dataEvento'); //Passando as variaves
         $descricao = $request->input('descricaoTexto');
+        
         //Inserindo os dados da tabela
-        $model = new modelAgenda();
+        $model = new modelAgenda(); // transformando em objeto
         $model->dataEvento = $data;
-        $model->descricao = $descricao;
+        $model->descricao = $descricao; // a ordem importa
 
-        $model->save();//armazena
-        return redirect('/cadastrar');
+        $model->save();//armazena o objeto
+        
+        return redirect('/cadastrar'); //voltar
     }//fim metodo requisicao de cadastro
 }// todas as operaçoes do BD
